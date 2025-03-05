20817f157f31a2e1d13ce33c44dfba809104897a
Joey Wasin <wasin.ww1402@gmail.com>
2025-03-05 11:06:10 +0700

    build: init project of poc websocket

---
+++ api/src/main/java/com/poc/websocket/PocWebsocketApplication.java
@@ -0,0 +1,13 @@
+package com.poc.websocket;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PocWebsocketApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PocWebsocketApplication.class, args);
+	}
+
+}
