9f95bd546b02a25ed5569fe511b9e38c026b7301
Joey Wasin <wasin.ww1402@gmail.com>
2025-03-05 11:20:34 +0700

    feat: add root path

---
+++ websocket-api/src/main/java/com/poc/websocket/HelloWorld/HelloController.java
@@ -0,0 +1,12 @@
+package com.poc.websocket.HelloWorld;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+    @GetMapping("/")
+    public String index() {
+        return "Welcome PoC WebSocket";
+    }
+}
